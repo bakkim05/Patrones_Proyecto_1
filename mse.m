89da8117e4fa727aa74bcc57fc1e2044d94416df
DRoMarin <47992796+DRoMarin@users.noreply.github.com>
2020-05-15 21:49:14 -0600

    cross entropy y mse

---
+++ mse.m
@@ -0,0 +1,24 @@
+#funcion MSE
+
+
+classdef mse < handle
+  properties    
+    outputs=[];
+    gradient=[];
+  endproperties
+
+  methods
+    function s=mse()
+      s.outputs=[];
+      s.gradient=[];
+    endfunction
+
+    function y=error(s,ynet,ytrain)
+      s.outputs = norm((ynet-ytrain),2)^2/rows(ytrain);
+      y=s.outputs;
+      s.gradient = 2*(ynet-ytrain)/rows(ytrain);
+    endfunction
+
+  endmethods
+endclassdef
+
