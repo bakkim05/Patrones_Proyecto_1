0f393d484fe70ec63410412ae96debb70ac015ba
DRoMarin <47992796+DRoMarin@users.noreply.github.com>
2020-05-16 18:46:32 -0600

    red con create_data

--- ANN.m
+++ ANN.m
@@ -6,6 +6,9 @@
 ## Tecnológico de Costa Rica
 
 
+
+[X,Y] = create_data(4,3, "vertical");
+
 ## Capas:
 l1a=fullyconnected();
 l1b=sigmoide();
@@ -22,25 +25,27 @@ l4b=softMax();
 
 ## Forward prop
 
-x=[1 2 3 4]';
-W1=[0.1  0.3 -0.1 0;-0.2 0.5 0.2 0.1; 1 0 0.1 -0.3; 0.5 0.6 0.8 -0.9];
-W2=[0.1  0.3 -0.1 0;-0.2 0.5 0.2 0.1; 1 0 0.1 -0.3; 0.5 0.6 0.8 -0.9;  0.5 0.6 0.8 -0.9];
-W3=[0.1  0.3 -0.1 0 2;-0.2 0.5 0.2 1 3;0 0.1 -0.3 0 1];
-W4=[0.1  0.3 -0.1;-0.2 0.5 0.2 ;0 0.1 -0.3];
-yt = [5 6 7]';
+x  = X(:,1);
+x  = [1; x];
+W1 = [0.1  0.3 -0.1 0 1;-0.2 0.5 0.2 0.1 1; 1 0 0.1 -0.3 1; 0.5 0.6 0.8 -0.9 1];
+W1 = [0.1  0.3 -0.1 0 1;-0.2 0.5 0.2 0.1 1; 1 0 0.1 -0.3 1; 0.5 0.6 0.8 -0.9 1];
+W1 = [0.1  0.3 -0.1 0 1;-0.2 0.5 0.2 0.1 1; 1 0 0.1 -0.3 1; 0.5 0.6 0.8 -0.9 1];
+W4 = [0.1  0.3 -0.1 1;-0.2 0.5 0.2 1;0 0.1 -0.3 1];
+yt = Y(1:3,2);
 
+counter =0;
+while counter < 1500
 y1a=l1a.forward(W1,x);
 y1b=l1b.forward(y1a);
-
+y1b=[1;y1b];
 y2a=l2a.forward(W2,y1b);
 y2b=l2b.forward(y2a);
-
+y2b=[1;y2b];
 y3a=l3a.forward(W3,y2b);
 y3b=l3b.forward(y3a);
-
+y3b=[1;y3b];
 y4a=l4a.forward(W4,y3b);
 y4b=l4b.forward(y4a);
-
 
 J = xent();
 error = J.error(y4b,yt);
@@ -52,17 +57,24 @@ gradJ = J.gradient;
 
 l4b.backward(gradJ); # asumiendo que esto es el final
 l4a.backward(l4b.gradient);
-l3b.backward(l4a.gradientX);
+l3b.backward(l4a.gradientX(2:end));
 l3a.backward(l3b.gradient);
-l2b.backward(l3a.gradientX);
+l2b.backward(l3a.gradientX(2:end));
 l2a.backward(l2b.gradient);
-l1b.backward(l2a.gradientX);
+l1b.backward(l2a.gradientX(2:end));
 l1a.backward(l1b.gradient);
 
 
 
-alpha = 0.05; ################################
-W1 = W1 + alpha*l1a.gradientW;
-W2 = W2 + alpha*l2a.gradientW;
-W3 = W3 + alpha*l3a.gradientW;
-W4 = W4 + alpha*l4a.gradientW;
+alpha = 0.08; ################################
+W1 = W1 - alpha*l1a.gradientW;
+W2 = W2 - alpha*l2a.gradientW;
+W3 = W3 - alpha*l3a.gradientW;
+W4 = W4 - alpha*l4a.gradientW;
+counter =counter +1;
+counter
+end
+y4b
+yt
+
+
