2da5621757a29032243425e0dfebf1e6ced120e7
Jung <bakkim05@gmail.com>
2020-05-17 18:47:18 -0600

    clase adam -> funcion adam

--- adam.m
+++ adam.m
@@ -1,43 +1,6 @@
 #!/usr/bin/octave-cli
-
-classdef adam < handle
-  properties
-    vdW = [];
-    sdW = [];
-    weight = []
-  endproperties
-  
-  methods
-    function w = adam()
-      w.vdW = [];
-      w.sdW = [];
-      w.weight = [];
-    endfunction
-    
-    function f = WeightedAvgPrevW (Beta, gradientW)
-      w.vdW = Beta*w.vdW + (1-Beta)*gradientW;
-      f = w.vdW;
-    endfunction
-    
-    function f = WeightedAvgSqrPrevW (Beta, gradientW);    
-      w.sdW = Beta*w.sdW + (1-Beta)*(gradientW.*gradientW);
-      f = w.sdW;
-    endfunction
-    
-    function f = theta(alfa)  #falta implementar esta funcion correctamente.
-      #w.weight = w.weight - alfa /
-    endfunction
-    
-    function h = compute(alfa, gradiente, beta1 = 0.9, beta2 = 0.99)
-      w.WeightedAvgPrevW (beta1, gradient);
-      w.WeightedAvgSqrPrevW (beta2, gradient);
-      w.theta(alfa);
-    endfunction
-    
-  endmethods
-  
-  
-  
-  
-  
-endclassdef
+function [theta,vt,st] = adam (alfa, theta, gradiente, vt, st, beta1 = 0.9, beta2 = 0.99, epsilon = 10^-8)
+  vt = beta1*vt + (1-beta1)*gradiente;
+  st = beta2*st + (1-beta2)*(gradiente.*gradiente);
+  theta = theta - (alfa ./ diag(sqrt(st + epsilon*eye(size(st)))))*vt;
+endfunction
