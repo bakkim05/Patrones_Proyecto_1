9701a53c40f1e81ecf78f7d2100393b67eb820ea
ozarias <54919935+ozarias@users.noreply.github.com>
2020-05-18 23:00:47 -0600

    Add files via upload
    funcion para poder evaluar cuantas predicciones son correctas y cuantas no

---
+++ eval.m
@@ -0,0 +1,17 @@
+function [aciertos,errores]=eval(correct,approx)
+  aciertos=0;
+  errores=0;
+  filas=length(correct);
+  act=1;
+  while act<filas+1
+  [q,A]=max(correct(act,:));
+  [q,B]=max(approx(act,:));
+  if [A==B]
+    aciertos+=1;
+  else
+    errores+=1;
+  end
+  act+=1;
+  
+  end
+endfunction
