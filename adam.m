6d881d1569d09dcfa84b127090a7309fdb99312d
DRoMarin <47992796+DRoMarin@users.noreply.github.com>
2020-05-19 18:49:36 -0600

    solucion adam

--- adam.m
+++ adam.m
@@ -1,6 +1,5 @@
-#!/usr/bin/octave-cli
 function [theta,vt,st] = adam (alfa, theta, gradiente, vt, st, beta1 = 0.9, beta2 = 0.99, epsilon = 10^-8)
   vt = beta1*vt + (1-beta1)*gradiente;
-  st = beta2*st + (1-beta2)*(gradiente.*gradiente);
-  theta = theta - (alfa ./ diag(sqrt(st + epsilon*eye(size(st)))))*vt;
+  st = beta2*st + (1-beta2)*(gradiente)^.2;
+  theta = theta - alpha*(st+e).^-1.*vt;
 endfunction
