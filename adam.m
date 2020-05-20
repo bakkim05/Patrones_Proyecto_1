4dd5d8dfd5b874c25ddb41f82e2097ef9f748efb
ozarias <54919935+ozarias@users.noreply.github.com>
2020-05-19 19:36:39 -0600

    Add files via upload
    alfa----> alpha

--- adam.m
+++ adam.m
@@ -1,5 +1,5 @@
-function [theta,vt,st] = adam (alfa, theta, gradiente, vt, st, beta1 = 0.9, beta2 = 0.99, epsilon = 10^-8)
+function [theta,vt,st] = adam (alpha, theta, gradiente, vt, st, beta1 = 0.9, beta2 = 0.99, epsilon = 10^-8)
   vt = beta1*vt + (1-beta1)*gradiente;
-  st = beta2*st + (1-beta2)*(gradiente)^.2;
+  st = beta2*st + (1-beta2)*(gradiente).^2;
   theta = theta - alpha*(st+e).^-1.*vt;
 endfunction
