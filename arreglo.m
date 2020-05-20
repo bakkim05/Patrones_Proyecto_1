32eee388e51b116f069ca58fda6619b9b704cf3e
ozarias <54919935+ozarias@users.noreply.github.com>
2020-05-19 19:36:12 -0600

    Add files via upload
    con momentum y adam

--- arreglo.m
+++ arreglo.m
@@ -8,23 +8,23 @@
 1;
 pkg load specfun;
 pkg load statistics;
-n=10; #muestras
-nw1 = 5; #peso capa 1
-nw2 = 5;#peso capa 2
+n=1000; #muestras
+nw1 = 10; #peso capa 1
+nw2 = 6;#peso capa 2
 nw3 = 5;#peso capa 3
 c=5; #clases
-[X,Y] = create_data(n,c, "vertical");
+[X,Y] = create_data(n,c, "pie");
 
 ## Capas:
 l1a=fullyconnected();
-l1b=sigmoide();
+l1b=leaky();
 l2a=fullyconnected();
 l2b=leaky();
 l3a=fullyconnected();
-l3b=ReLU();
+l3b=leaky();
 l4a=fullyconnected();
-#l4b=sigmoide();
-l4b=softMax();
+l4b=sigmoide();
+#l4b=softMax();
 
 ## Forward prop
 x  = X;
@@ -35,8 +35,20 @@ W3 = w_gen(nw2+1,nw3)';
 W4 = w_gen(nw3+1,c)';
 yt = Y;
 
-counter =0;
-while counter < 1000
+counter =1;
+v1=0;
+v2=0;
+v3=0;
+v4=0;
+s1=0;
+s2=0;
+s3=0;
+s4=0;
+alpha = 0.26;
+bet=0.06;
+val_saved=[];
+
+while counter < 30000
 y1a=l1a.forward(W1,x);
 y1b=l1b.forward(y1a);
 
@@ -55,11 +67,15 @@ y3b=[ones(rows(y3b),1) y3b];
 y4a=l4a.forward(W4,y3b);
 y4b=l4b.forward(y4a);
 
-#J = mse();
-J = xent();
-
+J = mse();
+#J = xent();
 error = J.error(y4b,yt);
 gradJ = J.gradient;
+
+if mod(counter,1000)==0
+  error
+  end
+
 ## Backprop.
 
 l4b.backward(gradJ); # asumiendo que esto es el final
@@ -74,18 +90,30 @@ l2a.backward(l2b.gradient);
 l1b.backward(l2a.gradientX(:,2:end));
 l1a.backward(l1b.gradient);
 
-alpha = 0.05; ################################
 #W1 = W1 - alpha*l1a.gradientW;
 #W2 = W2 - alpha*l2a.gradientW;
 #W3 = W3 - alpha*l3a.gradientW;
 #W4 = W4 - alpha*l4a.gradientW;
 
-W1 = W1 - alpha*l1a.gradientW;
-W2 = W2 - alpha*l2a.gradientW;
-W3 = W3 - alpha*l3a.gradientW;
-W4 = W4 - alpha*l4a.gradientW;
+[v1,W1]=momentum(v1,W1,l1a.gradientW,alpha,bet);
+[v2,W2]=momentum(v2,W2,l2a.gradientW,alpha,bet);
+[v3,W3]=momentum(v3,W3,l3a.gradientW,alpha,bet);
+[v4,W4]=momentum(v4,W4,l4a.gradientW,alpha,bet);
 
+#[W1,v1,s1]=adam(alpha,W1,l1a.gradientW,v1,s1);
+#[W2,v2,s2]=adam(alpha,W2,l2a.gradientW,v2,s2);
+#[W3,v3,s3]=adam(alpha,W3,l3a.gradientW,v3,s3);
+#[W4,v4,s4]=adam(alpha,W4,l4a.gradientW,v4,s4);
+
+val_saved(counter,1) = counter;
+val_saved(counter,2) = error;
 counter += 1;
+
+if error<0.015
+  counter
+  error
+  break
+  end
 end
-y4b
-yt
+
+[C,E,fails]=comparar(yt,y4b)
