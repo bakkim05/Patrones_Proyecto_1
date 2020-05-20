9c3b9b9f80cce9c41e188a2eb2f5de5716b6bff6
ozarias <54919935+ozarias@users.noreply.github.com>
2020-05-19 18:51:44 -0600

    Add files via upload
    Esta eval guarda los indices

--- eval.m
+++ eval.m
@@ -1,8 +1,9 @@
-function [aciertos,errores]=eval(correct,approx)
+function [aciertos,errores,failindex]=eval(correct,approx)
   aciertos=0;
   errores=0;
   filas=length(correct);
   act=1;
+  failindex=[];
   while act<filas+1
   [q,A]=max(correct(act,:));
   [q,B]=max(approx(act,:));
@@ -10,8 +11,10 @@ function [aciertos,errores]=eval(correct,approx)
     aciertos+=1;
   else
     errores+=1;
+    failindex=[failindex act];
   end
   act+=1;
   
   end
 endfunction
+
