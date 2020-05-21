51d6e2a4ca1c726d6bb8d60aa8c445d60bbbff3a
Oscar <arias.gamerozz@gmail.com>
2020-05-20 23:19:42 -0600

    normaliza despues de la salida

--- main.m
+++ main.m
@@ -10,8 +10,8 @@ x = [ones(rows(X),1) X];
 yt = Y;
 Red=model();
 [yp]=Red.train(x,yt);
-[C,E,fails]=comparar(yt,yp)
-disp(7);
+#[C,E,fails]=comparar(yt,yp)
+yp=normMatrix(yp);
 
 datosval=400;
 [Xv,Yv] = create_data(datosval,c,forma);
