cd3ce44a7a2160fc697096043c09d192d191623e
ozarias <54919935+ozarias@users.noreply.github.com>
2020-05-20 23:07:09 -0600

    normalizar matriz

---
+++ normMatrix.m
@@ -0,0 +1,11 @@
+function [Mn]=normMatrix(mat)
+  Mn=[];
+  
+  for i=1:rows(mat)
+  row=[zeros(1,columns(mat))];
+  [num,ind]=max(mat(i,:));  
+  row(ind)=1;    
+  Mn=[Mn;row];  
+    end
+  
+endfunction
